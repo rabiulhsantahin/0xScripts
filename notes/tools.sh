1224130de65bb8d6eeb1c339550b1f2bd056265d
Rabiul Hasan Tahin <rhtahins@gmail.com>
2021-02-14 20:20:28 +0600

    Update tools.sh

--- notes/tools.sh
+++ notes/tools.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+sudo -s
+apt-get update
 
-sudo apt update
-sudo apt install nmap
-sudo apt install gobuster
-sudo apt install dirb
-sudo apt install xfce4-netload-plugin #Network Traffic Monitor
+apt-get install golang
+apt-get install php
+apt-get install python
+apt-get install sublit3r
+apt-get install lynx
+apt-get install cewl
+apt-get install hydra
+apt-get install whatweb
+apt-get install cadaver
+apt-get install exploitdb
+apt-get install masscan
+apt-get install wfuzz
+apt-get install dirb
+apt-get install dnsenum
+apt-get install nmap
+apt-get install gobuster
+apt-get install html2text
+
+apt-get install xfce4-netload-plugin #Network Traffic Monitor
