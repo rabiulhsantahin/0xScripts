fd2b5364926d3613a6782026580e4c0d162e873d
Rabiul Hasan Tahin <rhtahins@gmail.com>
2021-02-15 16:33:17 +0600

    Update tools.sh

--- notes/tools.sh
+++ notes/tools.sh
@@ -2,29 +2,39 @@
 
 sudo apt-get update 
 
-sudo apt-get install cadaver -y
-sudo apt-get install cewl -y
-sudo apt-get install dirb -y
-sudo apt-get install dnsenum -y
-sudo apt-get install exploitdb -y
-sudo apt-get install gobuster -y
-sudo apt-get install golang -y
-sudo apt-get install html2text -y
-sudo apt-get install hydra -y
-sudo apt-get install lynx -y
-sudo apt-get install masscan -y
-sudo apt-get install nmap -y
-sudo apt-get install php -y
-sudo apt-get install python -y
-sudo apt-get install sublit3r -y
-sudo apt-get install wfuzz -y
-sudo apt-get install whatweb -y
+sudo apt-get install -y cadaver
+sudo apt-get install -y cewl
+sudo apt-get install -y dirb
+sudo apt-get install -y dnsenum
+sudo apt-get install -y exiftool
+sudo apt-get install -y exploitdb
+sudo apt-get install -y gobuster
+sudo apt-get install -y golang
+sudo apt-get install -y Hashcat
+sudo apt-get install -y html2text
+sudo apt-get install -y hydra
+sudo apt-get install -y john
+sudo apt-get install -y lynx
+sudo apt-get install -y masscan
+sudo apt-get install -y metasploit
+sudo apt-get install -y nmap
+sudo apt-get install -y php
+sudo apt-get install -y python
+sudo apt-get install -y python3-pip
+sudo apt-get install -y searchsploit
+sudo apt-get install -y steghide
+sudo apt-get install -y sublit3r
+sudo apt-get install -y tcpdump
+sudo apt-get install -y wfuzz
+sudo apt-get install -y whatweb
+sudo apt-get install -y wireshark
 
 ###Go applications###
+
 go get -u github.com/tomnomnom/assetfinder
 go get -u github.com/tomnomnom/gf
 go get -u github.com/tomnomnom/httprobe
 go get -u github.com/1ndianl33t/urlprobe
 go get -u github.com/tomnomnom/waybackurls
 
-sudo apt-get install xfce4-netload-plugin #Network Traffic Monitor
+# sudo apt-get install xfce4-netload-plugin #Network Traffic Monitor
