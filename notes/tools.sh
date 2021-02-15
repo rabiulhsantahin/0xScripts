ddac58eb4ca54694dff0a97557e9f4ecc5f41054
Rabiul Hasan Tahin <rhtahins@gmail.com>
2021-02-15 16:11:55 +0600

    Update tools.sh

--- notes/tools.sh
+++ notes/tools.sh
@@ -2,22 +2,29 @@
 sudo -s
 apt-get update
 
+apt-get install cadaver
+apt-get install cewl
+apt-get install dirb
+apt-get install dnsenum
+apt-get install exploitdb
+apt-get install gobuster
 apt-get install golang
+apt-get install html2text
+apt-get install hydra
+apt-get install lynx
+apt-get install masscan
+apt-get install nmap
 apt-get install php
 apt-get install python
 apt-get install sublit3r
-apt-get install lynx
-apt-get install cewl
-apt-get install hydra
-apt-get install whatweb
-apt-get install cadaver
-apt-get install exploitdb
-apt-get install masscan
 apt-get install wfuzz
-apt-get install dirb
-apt-get install dnsenum
-apt-get install nmap
-apt-get install gobuster
-apt-get install html2text
+apt-get install whatweb
+
+###Go applications###
+go get -u github.com/tomnomnom/assetfinder
+go get -u github.com/tomnomnom/gf
+go get -u github.com/tomnomnom/httprobe
+go get -u github.com/1ndianl33t/urlprobe
+go get -u github.com/tomnomnom/waybackurls
 
 apt-get install xfce4-netload-plugin #Network Traffic Monitor
