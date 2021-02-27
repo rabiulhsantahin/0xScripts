ba37573bd8c9a39baab51c8b5eb675016e35656c
Rabiul Hasan Tahin <rhtahins@gmail.com>
2021-02-28 00:49:05 +0600

    Update ss-otol.py

--- python/ss-otol.py
+++ python/ss-otol.py
@@ -1,10 +1,13 @@
 ### Code Writer: Otol Khan
 #get_screenshot_as_file
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-driver = webdriver.Chrome()
+import time
+import pyautogui
 
-driver.get("https://www.youtube.com")
+driver = webdriver.Chrome()
+driver.get("URL")
 #login cred
 '''
 username = driver.find_element_by_name("username")
@@ -15,9 +18,8 @@ password.clear()
 password.send_keys("test_pass")
 driver.find_element_by_name("btn_submit").click()
 '''
-#driver.get("https://URL") #pdf location
-body=driver.find_element_by_tag_name('body')
+time.sleep(45)
 for i in range(1,105):
-	driver.get_screenshot_as_file(f"{i}.png")
-	body.send_keys(Keys.ARROW_DOWN)
-  
+        driver.get_screenshot_as_file(f"{i}.png")
+        pyautogui.click(2458,565) # pyautogui.position() #for position#
+        time.sleep(1)
